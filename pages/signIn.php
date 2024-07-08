14c944e4f58964fb28d5fd713535b6ab87dffdba
Lukasz Karlinski <mysticsk733@gmail.com>
2024-07-08 21:03:13 +0200

    add singIn page

---
+++ pages/signIn.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <div class='h-screen w-full flex justify-center items-center bg-neutral-900 text-neutral-200'>
+        <div class='w-1/4 border-2 py-10 px-5 rounded-3xl'>
+            <h1 class='uppercase text-center text-3xl font-bold tracking-wider my-3'>rejestracja</h1>
+            <form action="includes/signInHandle.php" method="post" class='flex flex-col'>
+                <div class='flex flex-col my-3'>
+                    <label for="username" class='mb-1 capitalize'>username</label>
+                    <input type="text" name="username" class='outline-0 bg-neutral-600 text-neutral-300 rounded-xl py-1 px-2 ring-neutral-300 focus:ring-2'>
+                </div>
+                <div class='flex flex-col my-3' >
+                    <label for="password" class='mb-1 capitalize'>password</label>
+                    <input type="password" name="password" class='outline-0 bg-neutral-600 text-neutral-300 rounded-xl py-1 px-2 ring-neutral-300 focus:ring-2'>
+                </div>
+                <div class='flex flex-col my-3'>
+                    <label for="email" class='mb-1 capitalize'>e-mail</label>
+                    <input type="text" name="email" class='outline-0 bg-neutral-600 text-neutral-300 rounded-xl py-1 px-2 ring-neutral-300 focus:ring-2'>
+                </div>
+                <button class='bg-neutral-200 text-neutral-900 rounded-xl mt-2 px-5 py-2 mx-auto uppercase font-bold'>zarejestruj</button>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
