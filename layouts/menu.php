65efbd72486571a22a92a45372fb653b73b7f333
Lukasz Karlinski <mysticsk733@gmail.com>
2024-07-09 13:16:17 +0200

    add login page

--- layouts/menu.php
+++ layouts/menu.php
@@ -1,7 +1,7 @@
 <nav class='w-full bg-neutral-200'>
     <ul class='flex justify-evenly uppercase font-bold text-sm text-neutral-900'>
         <li class='px-6 py-3 cursor-pointer'><a href="index.php">strona główna</a></li>
-        <li class='px-6 py-3 cursor-pointer'><a href="">logowanie</a></li>
+        <li class='px-6 py-3 cursor-pointer'><a href="login.php">logowanie</a></li>
         <li class='px-6 py-3 cursor-pointer'><a href="signUp.php">rejestracja</a></li>
     </ul>
 </nav>
